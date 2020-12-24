d3acd1413bc03d69d16fc456adf882b5c5caf08f
paulc <paulchenau@163.com>
2020-12-25 00:37:44 +1100

    add time to bytes

--- pingClient.go
+++ pingClient.go
@@ -4,11 +4,14 @@ import (
 	//"golang.org/x/net/icmp"
 	"fmt"
 	"log"
+	"math/rand"
 	"net"
 	"reflect"
 	"sync"
+	"time"
 
 	"golang.org/x/net/icmp"
+	"golang.org/x/net/ipv4"
 )
 
 var (
@@ -30,6 +33,10 @@ type PingClient struct {
 	mut     *sync.Mutex            // mutex lock
 	ctx     *context               // controls stop or done
 	source  string                 // address source for icmp.ListenPacket
+	hasIpv4 bool                   // has ipv4 in addrs
+	hasIpv6 bool                   // has ipv6 in addrs
+	id      int                    // same id for icmp.Echo
+	seq     int                    // same seq for icmp.Echo
 	debug   bool                   // debug mode, print debug info to stdout
 }
 
@@ -44,15 +51,74 @@ func NewPingClient() *PingClient {
 	}
 }
 
+// Todo: add Ipv6 support
 func (p *PingClient) run(once bool) {
-	p.debugln("run(): Starting")
-	p.listen(ipv4Proto[p.network], p.source)
+	p.debugln("run(): Starting...")
+
+	var conn *icmp.PacketConn
+	if p.hasIpv4 {
+		if p.debug {
+			p.debugln("listen(): Ipv4 endpoint listening...")
+		}
+		conn = p.listen(ipv4Proto[p.network], p.source)
+		if conn == nil {
+			return
+		}
+		defer conn.Close()
+	}
+
 }
 
+func (p *PingClient) sendICMPpackets(conn *icmp.PacketConn) {
+	var typ icmp.Type
+	var bod icmp.MessageBody
+
+	typ = ipv4.ICMPTypeEcho
+	p.id = rand.Intn(0xffff) // 65535
+	p.seq = rand.Intn(0xffff)
+
+	for key, addr := range p.addrs {
+		now := time.Now()
+		// An Echo represents an ICMP echo request or reply message body.
+		bod = &icmp.Echo{
+			ID:   p.id,
+			Seq:  p.seq,
+			Data: timeToBytes(now),
+		}
+
+		// type echo code 0 no code
+		IcmpMsg := &icmp.Message{
+			Type: typ,
+			Code: 0,
+			Body: bod,
+		}
+
+	}
+}
+
+// For non-privileged datagram-oriented ICMP endpoints
+// Examples:
+//	p.listen("udp4", "192.168.0.1") ListenPacket("udp4", "192.168.0.1")
+//	ListenPacket("udp4", "0.0.0.0")
+//	ListenPacket("udp6", "fe80::1%en0")
+//	ListenPacket("udp6", "::")
+//
+// For privileged raw ICMP endpoints, network must be "ip4" or "ip6"
+// followed by a colon and an ICMP protocol number or name.
+//
+// Examples:
+//	ListenPacket("ip4:icmp", "192.168.0.1")
 func (p *PingClient) listen(netProto string, source string) *icmp.PacketConn {
 	conn, err := icmp.ListenPacket(netProto, source)
 	if err != nil {
+		p.mut.Lock()
+		defer p.mut.Unlock()
+		if p.debug {
+			p.debugln("listen(): error")
+		}
+		p.ctx.err <- true
 
+		return nil
 	}
 
 	return conn
@@ -73,11 +139,13 @@ func (p *PingClient) AddIP(s string) error {
 		p.addrs[ipaddr.String()] = &net.IPAddr{
 			IP: ipaddr,
 		}
+		p.hasIpv4 = true
 	} else if isIpv6(ipaddr) {
 		// Todo zone check(ipv6 may include zone %eth0)
 		p.addrs[ipaddr.String()] = &net.IPAddr{
 			IP: ipaddr,
 		}
+		p.hasIpv6 = true
 	}
 	return nil
 }
@@ -109,6 +177,27 @@ func getIP(s string) ([]net.IP, error) {
 		}
 	}
 	return ips, err
+}
+
+// copy from fastping
+// simply transfrom Time(underlying int64 timestamp) to byte array of length 8
+func timeToBytes(t time.Time) []byte {
+	nsec := t.UnixNano()
+	b := make([]byte, 8)
+	for i := uint8(0); i < 8; i++ {
+		fmt.Println((nsec >> ((7 - i) * 8)))
+		fmt.Println((nsec >> ((7 - i) * 8)) & 0xff)
+		b[i] = byte((nsec >> ((7 - i) * 8)) & 0xff)
+	}
+	return b
+}
+
+func bytesToTime(b []byte) time.Time {
+	var nsec int64
+	for i := 0; i < 8; i++ {
+		nsec += int64(b[i]) << ((7 - i) * 8)
+	}
+	return time.Unix(nsec/1000000000, nsec%1000000000)
 }
 
 // Ipv4 check since Ipv4 could also be 16-byte
